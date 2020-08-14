dab05b69a480f7e78e20669cc8520da2a3526338
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-08-14 16:45:50 +0530

    Add files via upload

---
+++ file2.java
@@ -0,0 +1,20 @@
+package pac1;
+import java.util.Scanner;
+public class file2 {
+	public static void main(String args[])
+	{
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter number 1");
+		int a=obj.nextInt();
+		System.out.println("enter number 2");
+		int b=obj.nextInt();
+		int c = a+b;
+		System.out.println("sum ="+c);
+		
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
