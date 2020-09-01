fe51893a0de8476d4e3a58a9016d41cad8360d11
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-01 22:44:56 +0530

    Add files via upload

---
+++ Nth_fibo.java
@@ -0,0 +1,26 @@
+package basic_prgs;
+
+import java.util.Scanner;
+
+public class Nth_fibo {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter number:");
+		int n = obj.nextInt();
+		int a=0, b=1, c=0, sum=a+b;
+		for(int i=2; i<=n;i++)
+		{
+			c=a+b;
+			sum = sum + c;
+			a=b;
+			b=c;
+			
+		}
+		System.out.println("Nth fiboncci for a given number: "+c);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
