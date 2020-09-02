831ce53e751542ff70b270d8bbd82dc9bd7cd038
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-02 11:23:22 +0530

    Add files via upload

---
+++ Cuboid.java
@@ -0,0 +1,24 @@
+package basic_prgs;
+import java.util.*;
+
+public class Cuboid {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter length:");
+		int L = sc.nextInt();
+		System.out.println("enter bredth:");
+		int b = sc.nextInt();
+		System.out.println("enter height:");
+		int h = sc.nextInt();
+		double a = L*b*h;
+		System.out.println("volume of cuboid = "+a);
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
