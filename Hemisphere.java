7a4020700aaf07a24a96cc287efc75ccea892e1d
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-02 11:22:53 +0530

    Add files via upload

---
+++ Hemisphere.java
@@ -0,0 +1,18 @@
+package basic_prgs;
+import java.util.*;
+
+public class Hemisphere {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter Radius:");
+		int r = obj.nextInt();
+		double a = (2.0/3.0)*(3.14)*(r*r*r);
+		System.out.println("volume of hemisphere = "+a);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
