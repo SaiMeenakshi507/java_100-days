5af917cf8f8d635947493b4ac079181541841a5a
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-02 11:22:22 +0530

    Add files via upload

---
+++ Rhombus.java
@@ -0,0 +1,20 @@
+package basic_prgs;
+
+import java.util.Scanner;
+
+public class Rhombus {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("enter two digits:");
+		int s1 = sc.nextInt();
+		int s2 = sc.nextInt();
+		double a = (1.0/2.0)*s1*s2;
+		System.out.println(" area of rhombus = "+a);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
