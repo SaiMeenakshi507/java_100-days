5fc2b138b2a53551cccb443b0a2f7434ef1ac80e
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-14 21:18:21 +0530

    Add files via upload

---
+++ Automorphic_number.java
@@ -0,0 +1,26 @@
+package new1;
+import java.util.*;
+public class Automorphic_number {
+        public static void main(String[] args){
+        	Scanner sc = new Scanner(System.in);
+        	System.out.println("enter number :");
+        	int n = sc.nextInt();
+        	int x = 0;
+        	
+        	int sq = n*n;
+        	
+        	while(n>0)
+        	{
+        		if(n%10 != sq%10)
+        		{
+        			    x = 1;
+        			    break;
+        		}
+        		n= n/10;
+        		sq = sq/10;
+        	}
+        	if(x==0)
+        		   System.out.println("Automorphic Number");
+        	else
+        		   System.out.println("Not Automorphic Number");
+        }}
