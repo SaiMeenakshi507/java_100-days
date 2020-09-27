7cdf5546e5a8ac8ba4b2aa39be80437c053c21ef
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-09-27 21:04:08 +0530

    Add files via upload

---
+++ Sastry_number.java
@@ -0,0 +1,15 @@
+package numbers;
+import java.util.*;
+public class Sastry_number {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number:");
+	int n = sc.nextInt();
+	String str = Integer.toString(n)+Integer.toString(n+1);
+	int num = Integer.valueOf(str);
+	double sr = Math.sqrt(num);
+	if((sr-Math.floor(sr))==0)
+			System.out.println("sastry number");
+	else
+		System.out.println("not sastry number");
+	}}
