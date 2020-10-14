fe2912185c6e43922635a1ae76d603fcdec0a39f
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-10-14 11:02:47 +0530

    Add files via upload

---
+++ pattern26.java
@@ -0,0 +1,18 @@
+package patterens;
+
+public class pattern26{
+
+		public static void main(String[] args) {
+			int n=5;
+			for(int i=1; i<=5; i++)
+			{
+			for(int j= n-1; j>=i; j--)
+				System.out.print(" ");
+			for(int k=1; k <= i;k++)
+				System.out.print((char)(k+64));
+
+			System.out.println();
+			}
+	}
+	}
+
