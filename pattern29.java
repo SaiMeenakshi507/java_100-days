57ef05bdcbd3ffbc52836ce063e25d2f264a25de
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-10-22 20:03:26 +0530

    Add files via upload

---
+++ pattern29.java
@@ -0,0 +1,18 @@
+package patterens;
+
+public class pattern29 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=n; i>=1; i--)
+		{
+			for(int j=n-1; j>=i; j--)
+				System.out.print(" ");
+			for(int k=1; k<=i;k++)
+				System.out.print((char)(i+64));
+			System.out.println();
+		}
+
+	}
+
+}
