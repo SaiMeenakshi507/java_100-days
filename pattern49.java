d5cc5f06105c38e5f4994b47a4c33aeee1fa7411
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-10-23 18:39:30 +0530

    Add files via upload

---
+++ pattern49.java
@@ -0,0 +1,15 @@
+package pattern2;
+
+public class pattern49 {
+
+	public static void main(String[] args) {
+		int n=3;
+		for(int i=n; i>=-n; i--)
+		{
+			for(int j= n; j>= Math.abs(i); j--)
+				System.out.print("*");
+			System.out.println();
+	}
+	}
+
+}
