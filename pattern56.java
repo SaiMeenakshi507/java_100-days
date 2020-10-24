3c3aca793ec8970768e95348d2f6d0d3a9f6092c
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-10-24 23:47:25 +0530

    Add files via upload

---
+++ pattern56.java
@@ -0,0 +1,17 @@
+package pattern3;
+
+public class pattern56 {
+
+	public static void main(String[] args) {
+		int n=3;
+		for(int i=n; i>=-n; i--)
+		{
+			for(int j=1; j<= Math.abs(i); j++)
+				System.out.print(" ");
+			for(int k=Math.abs(i); k<=n; k++)
+				System.out.print(k);
+			System.out.println();
+	}
+	}
+
+}
