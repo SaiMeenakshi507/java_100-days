7326e8c70ffd35dabe501d3b24a32f3abf1d3563
SaiMeenakshi507 <69675721+SaiMeenakshi507@users.noreply.github.com>
2020-10-26 19:19:42 +0530

    Add files via upload

---
+++ pattern62.java
@@ -0,0 +1,17 @@
+package pattern3;
+
+public class pattern62 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=n; i>=1;i--)
+		{
+			for(int j=n-1; j>=i; j--)
+				System.out.print(" ");
+			for(int k=i; k>=1; k--)
+				System.out.print((char)(i+64)+" ");
+				System.out.println();
+		}
+	}
+
+}
